50c51af652692094a917c889539a9e9934292f1c
Friederike <friederike@schertel.org>
2021-04-16 14:13:22 +0200

    simplest plugin

---
+++ plugin.js
@@ -0,0 +1,17 @@
+ 'use strict';
+
+ /** @type {LH.Config.Plugin} */
+ module.exports = {
+   audits: [{
+	 path: 'lighthouse-plugin-example/audits/preload-as.js',
+   }],
+ 
+   category: {
+	 title: 'Web For Future',
+	 description: 'work in progress',
+	 auditRefs: [
+	   {id: 'webforfuture', weight: 1},
+	   {id: 'meta-description', weight: 1},
+	 ],
+   },
+ };
