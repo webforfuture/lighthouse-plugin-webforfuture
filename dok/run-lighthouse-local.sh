f0344cdbb247c2f6359cdfd470f592a3908c3c7c
Friederike <friederike@schertel.org>
2021-06-23 23:15:31 +0200

    test-script sollte sofort laufen können und nur dieses Plugin testen

--- dok/run-lighthouse-local.sh
+++ dok/run-lighthouse-local.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 mkdir -p /tmp/foo/
+
 NODE_PATH=.. lighthouse https://webforfuture.org \
   --plugins=lighthouse-plugin-webforfuture \
+  --only-categories='lighthouse-plugin-webforfuture' \
   --view \
   --chrome-flags="--no-sandbox --headless" \
   --output-path=/tmp/foo/webforfuture-report.html
