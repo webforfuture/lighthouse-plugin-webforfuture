5b6882fba7da952dc9476f0a5a97d684815ff906
Friederike <friederike@schertel.org>
2021-06-23 23:29:58 +0200

    testing another audit

---
+++ audits/second-audit.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { Audit, NetworkRecords } = require('lighthouse');
+
+class SecondAudit extends Audit {
+static get meta() {
+	return {
+		id: 'SecondAudit',
+		title: 'foo',
+		failureTitle: 'fooerr',
+		description: 'bar',
+		requiredArtifacts: [],
+	};
+}
+
+static async audit(artifacts, context) {
+	return {
+		score: 1,
+		displayValue: 'always 1',
+	};
+}
+}
+
+module.exports = SecondAudit;
--- plugin.js
+++ plugin.js
@@ -2,15 +2,16 @@
 
  /** @type {LH.Config.Plugin} */
  module.exports = {
-   audits: [{
-	 path: 'lighthouse-plugin-webforfuture/audits/pagesize-over-average.js',
-   }],
- 
-   category: {
+	audits: [
+		{path: 'lighthouse-plugin-webforfuture/audits/pagesize-over-average.js'},
+		{path: 'lighthouse-plugin-webforfuture/audits/second-audit.js'},
+	],
+
+	category: {
 	 title: 'Web For Future',
 	 description: 'Checking websites for sustainability criteria.',
 	 auditRefs: [
-	   {id: 'PageSizeOverAverageAudit', weight: 1},
+		{id: 'PageSizeOverAverageAudit', weight: 1},
 	 ],
    },
  };
